f98c74917bffc98c586d037af3c9fa7d5acf0f0a
Paingkhant0397 <paingkhant0397@gmail.com>
2024-06-12 14:33:49 +0630

    finished part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,35 @@
 const Header = (props) => {
 
-  console.log(props)
+  // console.log(props)
   return <h1>{props.course}</h1>
 }
 
-const Content = () => {
+const Content = ({ parts }) => {
   return (
     <div>
-
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
 
-const Total = () => {
+const Part = ({ name, exercises }) => {
   return (
-    <div>
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
 
-    </div>
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 }
 
 const App = () => {
+
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -41,9 +50,8 @@ const App = () => {
 
 
   return (
-
-
     <div>
+
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
